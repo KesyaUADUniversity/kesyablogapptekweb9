a72eb12110dee583cbbf382750118cb8bbe45c39
unknown <cacadecaca018@gmail.com>
2025-12-23 07:01:22 +0700

    build success & ready for github pages

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/kesyablogapptekweb9",
+  assetPrefix: "/kesyablogapptekweb9/",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
