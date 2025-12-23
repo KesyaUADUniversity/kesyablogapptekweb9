5057ca3e3345b7ae4fc92ead2fffe460b47514f9
unknown <cacadecaca018@gmail.com>
2025-12-23 07:56:03 +0700

    Updates

--- _next/static/GO4XvmNBAZXdwf38d3oIY/_buildManifest.js
+++ _next/static/GO4XvmNBAZXdwf38d3oIY/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/kesyablogapptekweb9//_next/:path+",
-        "destination": "/kesyablogapptekweb9/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
